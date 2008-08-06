6e12baaff5585f7a3aef6103a826d9bf270e2d03
Phillip Pollard <phil@crescendo.net>
2008-08-06 02:30:12 +0000

    Adding more cron jobs.

--- support/cron.d
+++ support/cron.d
@@ -1,1 +1,2 @@
 0 18 * * * horus cd /home/horus/bin && ./grab.pl --quiet --email=dcops@fusionone.com
+0 9 * * * horus cd /home/horus/bin && ./grab.pl --quiet --noconfigsave --email=ppollard@fusionone.com
