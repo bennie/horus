1cc812310103a37af6f2e9db5c012e01a14d6e88
Phillip Pollard <phil@crescendo.net>
2008-12-11 23:10:33 +0000

    Attempting connections to LDAP.

---
+++ bin/authtest.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use strict;
+
+use Crypt::PasswdMD5;
+use Crypt::SaltedHash;
+
+use Net::LDAP;
+
+my($ldap) = Net::LDAP->new('ldap.myhost.com') or die "Can't bind to ldap: $!\n";
+$ldap->bind("cn=manager,dc=mycompany,dc=com", password=>'mypassword');
+
+my $mesg = $ldap->search(filter => '(objectClass=*)', base => "ou=People,dc=mycompany,dc=com");
+
+print $mesg->code, ' ',  $mesg->error, "\n";
+
+for my $entry ($mesg->entries) {
+  my $uid = ($entry->get('uid'))[0];
+  my $pass = ($entry->get('userPassword'))[0];
+  my $valid = &check_pass($pass, 'fusion123');
+  print "$valid -> $uid $pass\n";
+}
+
+
+sub check_pass {
+  my $pass = shift @_;
+  my $text = shift @_;
+  if ( $pass =~ /^\{crypt\}(.+)$/i ) {
+    my $crypt = $1;
+    my $enc = unix_md5_crypt($text,$crypt);
+    return $enc eq $crypt ? 1:0;
+  }
+  return Crypt::SaltedHash->validate($pass, $text) ? 1:0;
+}
