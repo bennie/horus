435f82e0926214bf7cace593cb44fd3fb613db18
Phillip Pollard <phil@crescendo.net>
2014-04-11 16:39:40 -0700

    Making the connection more generic.

--- bin/authtest.pl
+++ bin/authtest.pl
@@ -6,34 +6,40 @@ use Data::Dumper;
 use Net::LDAP;
 use Term::ReadLine;
 use Term::ReadPassword;
+use strict;
 
 use LocalVars;
-
-use strict;
 
 $Term::ReadPassword::USE_STARS = 1;
 my $term = new Term::ReadLine ('pointless');
 
 print "Using the following settings:\n";
 print " * Host: $LocalVars::ldap_host\n";
-print " * User: $LocalVars::ldap_user\n";
+print " * Port: $LocalVars::ldap_port\n";
+print " * Bind: $LocalVars::ldap_bind\n";
 print " * Pass: $LocalVars::ldap_pass\n";
 print " * Base: $LocalVars::ldap_base\n\n";
 
-my $bind_dn = "cn=$LocalVars::ldap_user,$LocalVars::ldap_base";
-print " * Bind DN: $bind_dn\n\n";
+my $ldap = Net::LDAP->new($LocalVars::ldap_host, ($LocalVars::ldap_port?(port=>$LocalVars::ldap_port):()) )
+             or die "Can't connect to ldap server: $@\n";
 
-my $ldap = Net::LDAP->new($LocalVars::ldap_host) or die "Can't connect to ldap server: $@\n";
-
-my $ret = $ldap->bind( $bind_dn , password => "$localVars::ldap_pass" );
+my $ret = $ldap->bind( $LocalVars::bind_dn , password => "$LocalVars::ldap_pass" );
 die $ret->{errorMessage} . "\n" . Dumper($ret) if $ret->{errorMessage};
+
+unless ( $ret->error eq 'Success' ) {
+  print "\n", $ret->error, "\n", Dumper($ret);
+  exit 1;
+}
 
 my $user = $term->readline('user: ');
 my $password = read_password('password: ');
 
-my $mesg = $ldap->search(filter => "(uid=$user)", base => $LocalVars::ldap_base);
+my $mesg = $ldap->search(filter => "(sAMAccountName=$user)", base => $LocalVars::ldap_base);
 
-print $mesg->code, ' ',  $mesg->error, "\n";
+unless ( $mesg->error eq 'Success' ) {
+  print "\n", $mesg->error, "\n", Dumper($mesg);
+  exit 1;
+}
 
 for my $entry ($mesg->entries) {
   my $uid = ($entry->get('uid'))[0];
