1bcd8e327f9c87411a86b23261f7aca79533e689
Phillip Pollard <phillip@purestorage.com>
2014-04-12 21:43:04 -0700

    Closer

--- bin/authtest.pl
+++ bin/authtest.pl
@@ -1,7 +1,5 @@
-#!/usr/bin/perl -I../lib
+#!/usr/bin/env perl -I../lib
 
-use Crypt::PasswdMD5;
-use Crypt::SaltedHash;
 use Data::Dumper;
 use Net::LDAP;
 use Term::ReadLine;
@@ -18,12 +16,17 @@ print " * Host: $LocalVars::ldap_host\n";
 print " * Port: $LocalVars::ldap_port\n";
 print " * Bind: $LocalVars::ldap_bind\n";
 print " * Pass: $LocalVars::ldap_pass\n";
-print " * Base: $LocalVars::ldap_base\n\n";
+print " * Base: $LocalVars::ldap_base\n";
+print " * UID:  $LocalVars::ldap_uid\n\n";
 
-my $ldap = Net::LDAP->new($LocalVars::ldap_host, ($LocalVars::ldap_port?(port=>$LocalVars::ldap_port):()) )
+my $ldap = Net::LDAP->new($LocalVars::ldap_host, ($LocalVars::ldap_port?(port=>$LocalVars::ldap_port):()), async => 1 )
              or die "Can't connect to ldap server: $@\n";
 
-my $ret = $ldap->bind( $LocalVars::bind_dn , password => "$LocalVars::ldap_pass" );
+### Test search bind
+
+print "\n == Testing anonymous bind for searching.\n";
+
+my $ret = $ldap->bind( $LocalVars::ldap_bind , password => "$LocalVars::ldap_pass" );
 die $ret->{errorMessage} . "\n" . Dumper($ret) if $ret->{errorMessage};
 
 unless ( $ret->error eq 'Success' ) {
@@ -31,31 +34,41 @@ unless ( $ret->error eq 'Success' ) {
   exit 1;
 }
 
+print "\n SUCCESS: Able to bind with $LocalVars::ldap_bind\n";
+
+### Test searching
+
+print "\n == Testing search with this bind.\n";
+
+my $search_user = $term->readline('username to seatch for: ');
+
+my $search = $ldap->search(filter => "($LocalVars::ldap_uid=$search_user)", base => $LocalVars::ldap_base);
+
+unless ( $search->error eq 'Success' ) {
+  print "\n", $search->error, "\n", Dumper($search->entries);
+  exit 1;
+}
+
+print "Returned object: \n", Dumper($search);
+
+### Test a user login
+
+print "\n == Testing a user login:\n";
+
 my $user = $term->readline('user: ');
 my $password = read_password('password: ');
 
-my $mesg = $ldap->search(filter => "(sAMAccountName=$user)", base => $LocalVars::ldap_base);
+my $mesg = $ldap->search(filter => "($LocalVars::ldap_uid=$user)", base => $LocalVars::ldap_base);
 
 unless ( $mesg->error eq 'Success' ) {
   print "\n", $mesg->error, "\n", Dumper($mesg);
   exit 1;
 }
 
-for my $entry ($mesg->entries) {
-  my $uid = ($entry->get('uid'))[0];
-  my $pass = ($entry->get('userPassword'))[0];
-  my $valid = &check_pass($pass, $password);
-  print "$valid -> $uid $pass\n";
-}
+my $ou = ($mesg->entries())[0]->get('distinguishedName')->[0];
+$ldap->unbind;
 
-sub check_pass {
-  my $pass = shift @_;
-  my $text = shift @_;
-  if ( $pass =~ /^\{crypt\}(.+)$/i ) {
-    print "Local crypt check.\n";
-    my $crypt = $1;
-    my $enc = unix_md5_crypt($text,$crypt);
-    return $enc eq $crypt ? 1:0;
-  }
-  return Crypt::SaltedHash->validate($pass, $text) ? 1:0;
-}
+my $ret = $ldap->bind( $ou , password => $password );
+die $ret->{errorMessage} . "\n" . Dumper($ret) if $ret->{errorMessage};
+
+print $ret->error();
